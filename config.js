a54bb353facf845d3c24e7b57d6eb26e67ac98c6
cosmin <cs8803798@gmail.com>
2025-01-04 17:07:26 +0200

    Update config.js

--- config.js
+++ config.js
@@ -21,11 +21,11 @@ global.prefa = ["."];                                    //Default prefix here. 
 
 
 //
-global.Owner = ["+94702782946", "+94702782946"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.OwnerNumber = ["+94702782946", "+94702782946"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.ownertag = ["+94702782946"];
-global.OwnerName = "SACHITH";
-global.BotName = "SACHITH";
+global.Owner = ["0770811929", "0770811929"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.OwnerNumber = ["0770811929", "0770811929"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.ownertag = ["0770811929"];
+global.OwnerName = "ANONIM";
+global.BotName = "ANONIM";
 global.packname = "🦋 │𝐂𝐘𝐁𝜩𝐑│𝐏𝜟𝐍𝐃𝐀│𝐌𝐃│𝐕➂ 🦋";                             //Do not change.
 global.author = "SACHITH";                               //Do not change.
 global.BotSourceCode = "https://github.com/Kai00"; //Do not change.
